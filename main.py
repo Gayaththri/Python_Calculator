10e9afb0d79a83ff826f840b1f89ff5b7587619b
Gayaththri <sriigaya3@gmail.com>
2025-06-12 19:53:16 +0530

    Full python calculator

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ num1 = float(input("Enter the 1st number: "))
 num2 = float(input("Enter the 2nd number: "))
 
 if operator == "+":
-    pass
+    result = num1 + num2
+    print(round(result, 3))
 elif operator == "-":
-    pass
+    result = num1 - num2
+    print(round(result, 3))
 elif operator == "*":
-    pass
+    result = num1 * num2
+    print(round(result, 3))
 elif operator == "/":
-    pass
+    result = num1 / num2
+    print(round(result, 3))
+else:
+    print(f"{operator} is not a valid operator")
